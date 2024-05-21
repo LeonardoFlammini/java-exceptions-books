f2531ca977291634f79be9604b2c1b040fcff1ac
LeonardoFlammini <leonardo98flammini@gmail.com>
2024-05-21 14:56:05 +0200

    [add]scaffolding

---
+++ src/org/newlibrary/Book.java
@@ -0,0 +1,24 @@
+package org.newlibrary;
+
+public class Book {
+
+    //Attributes
+    private int number_of_pages;
+    private String author,title,publisher;
+
+    //Costructor
+    Book (){
+
+    }
+
+    public void setTitle(){
+
+    }
+
+    public void checkValue(){
+
+    }
+
+
+
+}
---
+++ src/org/newlibrary/Main.java
@@ -0,0 +1,7 @@
+package org.newlibrary;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
