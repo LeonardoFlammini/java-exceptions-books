d932e3b49ab4fa286cd3499a4cfbda032cd64947
LeonardoFlammini <leonardo98flammini@gmail.com>
2024-05-21 15:47:30 +0200

    First commit

--- src/org/newlibrary/Book.java
+++ src/org/newlibrary/Book.java
@@ -6,17 +6,79 @@ public class Book {
     private int number_of_pages;
     private String author,title,publisher;
 
-    //Costructor
-    Book (){
 
+
+    //Constructor
+    public Book (String title,String author,String publisher,int number_of_pages) throws IllegalArgumentException{
+        checkString(title, "Title");
+        checkString(author, "Author");
+        checkString(publisher, "Publisher");
+        checkPages(number_of_pages);
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.number_of_pages = number_of_pages;
     }
 
-    public void setTitle(){
 
+
+
+    //Setter
+    public void setTitle(String title) throws IllegalArgumentException{
+        checkString(title, "Title");
+        this.title = title;
+    }
+    public void setAuthor(String author) throws IllegalArgumentException{
+        checkString(author,"Author");
+        this.author = author;
+    }
+    public void setPublisher(String publisher) throws IllegalArgumentException{
+        checkString(publisher,"Publisher");
+        this.publisher = publisher;
+    }
+    public void setNumber_of_pages(int number_of_pages) throws NumberFormatException{
+        checkPages(number_of_pages);
+        this.number_of_pages = number_of_pages;
     }
 
-    public void checkValue(){
 
+
+    // Getter
+    public int getNumber_of_pages() {
+        return number_of_pages;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    //Methods
+    public String printBook(){
+        String book = String.format("{\nTitle: %s\nAuthor: %s\nPublisher: %s\nNumber of pages: %d\n}",title,author,publisher,number_of_pages);
+        return book;
+    }
+
+
+
+    //Exception
+    public void checkString(String s, String type) throws IllegalArgumentException{
+        if (s == null || s.isEmpty()){
+            throw new IllegalArgumentException("Invalid parameter " + type);
+        }
+    }
+
+    public void checkPages(int p){
+        if (p<=0){
+            throw new NumberFormatException("Invalid number of pages");
+        }
     }
 
 
--- src/org/newlibrary/Main.java
+++ src/org/newlibrary/Main.java
@@ -1,7 +1,40 @@
 package org.newlibrary;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = 3,number_of_pages;
+        String author, title, publisher;
+
+        Book[] booksArray = new Book[n];
+
+        for (int i = 0; i < n; i++) {
+            Book book = null;
+            while(book == null){
+                try {
+                    System.out.println("Insert title: ");
+                    title = scanner.nextLine();
+                    System.out.println("Insert author: ");
+                    author = scanner.nextLine();
+                    System.out.println("Insert publisher: ");
+                    publisher = scanner.nextLine();
+                    System.out.println("Insert number of pages: ");
+                    number_of_pages = Integer.parseInt(scanner.nextLine());
+                    book = new Book(title,author,publisher,number_of_pages);
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            booksArray[i] = book;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(booksArray[i].printBook());
+        }
 
     }
 }
