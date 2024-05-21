b1545e3278beac1f9eff7f09a63e76b6fea1b7c7
LeonardoFlammini <leonardo98flammini@gmail.com>
2024-05-21 16:16:50 +0200

    Final commit

--- src/org/newlibrary/Main.java
+++ src/org/newlibrary/Main.java
@@ -1,12 +1,16 @@
 package org.newlibrary;
 
-import java.util.Arrays;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        File filePath = new File("./resources/data.txt");
 
         int n = 3,number_of_pages;
         String author, title, publisher;
@@ -31,10 +35,31 @@ public class Main {
                 }
             }
             booksArray[i] = book;
-        }
-        for (int i = 0; i < n; i++) {
-            System.out.println(booksArray[i].printBook());
+            writeBooksToFile(book, filePath);
         }
 
+        readFile(filePath);
+
+        scanner.close();
+    }
+
+    private static void writeBooksToFile(Book book, File file) {
+        // try-with-resources
+        try (FileWriter fileWriter = new FileWriter(file, true)) { // Aggiunto 'true' per abilitare l'append
+            fileWriter.write(book.printBook() + "\n"); // Aggiunto '\n' per separare i libri nel file
+        } catch (IOException e) {
+            System.out.println("Unable to write to file: " + e.getMessage());
+        }
+    }
+
+    private static void readFile(File file) {
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to read file");
+        }
     }
 }
